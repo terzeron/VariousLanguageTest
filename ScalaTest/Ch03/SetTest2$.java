bcbf5806e95325a879e16a39234977271b3cc069
Young-il Cho <youngilcho@nhnent.com>
2016-11-14 16:35:46 +0900

    remove old files

--- ScalaTest/Ch03/SetTest2$.java
+++
@@ -1,7 +0,0 @@
-object SetTest2 {
-  def main(args: Array[String]) {
-    val movieSet = Set("Hitch", "Poltergeist")
-    movieSet += "Shrek" // val인데도 변경 가능
-    println(movieSet)
-  }
-}
--- ScalaTest/Ch04/src/Test01.scala
+++
@@ -1,6 +0,0 @@
-object Main {
-  def main(args: Array[String]): Unit = {
-    val s = "Hello, world!"
-    print(s toLowerCase)
-  }
-}
